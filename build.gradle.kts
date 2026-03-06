443728bad6b5ec3efc1e008754e36bd7b3ea92c1
danpasecinic <plam.developer@gmail.com>
2026-03-06 20:44:32 +0100

    chore: scaffold Kotlin project with Gradle
    Kotlin 2.1.20, JVM 21, JUnit 5 + mockito-kotlin for tests.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "dev.danpasecinic"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
